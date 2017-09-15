f7697b583fd55068355a514991f6fbc84d4a8d0f
LD MacKrell <ldmackrell@gmail.com>
2017-09-15 16:56:37 -0700

    update add with integration specs.

--- spec/kid_dictionary_integration_spec.rb
+++ spec/kid_dictionary_integration_spec.rb
@@ -1,10 +1,23 @@
 require('capybara/rspec')
 require('./app')
-Capybara.app = Sinatra:application
+Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('#clickable_links', {:type => :feature}) do
-  it("will link to a new page when clicked on by user") do
-    expect(Word.entry("mordant"))
+describe("#submit_word", {:type => :feature}) do
+  it("will add words to a dictionary list") do
+    visit('/')
+    fill_in('new_entry', :with => 'mordant')
+    click_button('Add!')
+    expect(page).to have_content("mordant")
   end
+
+  describe('#clickable_links', {:type => :feature}) do
+    it("will link to a new page when clicked on by user") do
+      visit('/')
+      click_link("mordant")
+      expect(page).to have_content("mordant")
+    end
+  end
+
+
 end
